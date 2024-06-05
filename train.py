7e231e9dac3b26a2618de320ec4db18a76c61b11
ethansmith2000 <ethansmith2000@gmail.com>
2024-06-05 08:48:05 +0000

    fixed loss func and stuff

--- train.py
+++ train.py
@@ -2,13 +2,14 @@ import torch
 import torchvision
 from torchvision import transforms
 from diffusers import UNet2DModel
-from torch.optim import AdamW
+from torch.optim import AdamW, Adam
 import diffusers
 from tqdm import tqdm
 
 def get_model(num_gaussians=3):
     # block_out_channels=(128, 128, 256, 256, 512, 512)
     block_out_channels=(128, 128, 256, 256, 512)
+    # block_out_channels=(256, 256, 256, 512, 1024)
     down_block_types=( 
         "DownBlock2D",  # a regular ResNet downsampling block
         "DownBlock2D", 
@@ -31,29 +32,47 @@ def get_model(num_gaussians=3):
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 size = 32
+num_gaussians=3
+batch_size=256
+lr = 0.4e-4
+pool_probs=True
+save_steps=500
+resume_ckpt = 'diffusion.ckpt'
+epochs = 300
+# resume_ckpt = None
+
 transform = transforms.Compose([transforms.Resize(size),
                                 transforms.CenterCrop(size), 
                                 transforms.RandomHorizontalFlip(0.5),transforms.ToTensor()])
-# train_dataset = torchvision.datasets.CelebA(root=CELEBA_FOLDER, split='train',
-#                                         download=True, transform=transform)
 train_dataset = torchvision.datasets.CIFAR10(root='./datasets/cifar10', train=True, download=True, transform=transform)
-dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, drop_last=True)
+dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, drop_last=True)
 
-num_gaussians=3
 
 model = get_model(num_gaussians=num_gaussians).to(device)
 scheduler = diffusers.DDIMScheduler.from_pretrained("runwayml/stable-diffusion-v1-5",subfolder="scheduler")
 
-optimizer = AdamW(model.parameters(), lr=1e-4, betas=(0.9, 0.98), weight_decay=0.01, eps=1e-8)
+optimizer = AdamW(model.parameters(), lr=lr, betas=(0.9, 0.98), weight_decay=0.01, eps=1e-8)
 nb_iter = 0
+
+if resume_ckpt:
+    state_dict = torch.load(resume_ckpt)
+    nb_iter = state_dict['step']
+    model.load_state_dict(state_dict['state_dict'])
+    print('Loaded checkpoint')
 
 
 def sample_mixprob(mixprob):
     b, num_g, c, h, w = mixprob.shape
-    mixprob = mixprob.permute(0, 2, 3, 4, 1).reshape(-1, num_g)
-    probabilities = torch.nn.functional.softmax(mixprob, dim=-1)
-    indices = torch.multinomial(probabilities, 1)
-    indices = indices.view(b, c, h, w)[:,None,...]
+    if pool_probs:
+        mixprob = mixprob.flatten(2).mean(-1)
+        mixprob = torch.nn.functional.softmax(mixprob, dim=-1)
+        indices = torch.multinomial(mixprob, 1)
+        indices = indices[:,:,None,None,None].repeat(1,1,c,h,w)
+    else:
+        mixprob = mixprob.permute(0, 2, 3, 4, 1).reshape(-1, num_g)
+        probabilities = torch.nn.functional.softmax(mixprob, dim=-1)
+        indices = torch.multinomial(probabilities, 1)
+        indices = indices.view(b, c, h, w)[:,None,...]
 
     return indices
 
@@ -61,14 +80,16 @@ def loss_fn(pred, target):
     # channnels goes [mean1_r, mean1_g, mean1_b, mean2_r... logvar1_r, logvar1_g, logvar1_b, logvar2_r..., mixprob1_r, mixprob1_g, mixprob1_b, mixprob2_r...]
     means, logvar, mixprob = torch.chunk(pred, 3, dim=1)
     means, logvar, mixprob = map(lambda x: x.view(x.shape[0], num_gaussians, 3, x.shape[2], x.shape[3]).flatten(2), [means, logvar, mixprob])
+    if pool_probs:
+        mixprob = mixprob.mean(-1, keepdim=True)
     target = target.flatten(1)[:,None,:].repeat(1,num_gaussians,1)
 
     stds = (logvar.exp() + 1e-8).sqrt()
     diff = (target - means)
-    mixprob = mixprob.softmax(-1)
+    mixprob = mixprob.softmax(-2)
 
     log_prob = -0.5 * (diff / stds) ** 2 - torch.log(stds) - 0.5 * torch.log(2 * torch.tensor(3.1415))
-    log_probs = (log_prob + torch.log(mixprob + 1e-8)).logsumexp(-1)
+    log_probs = (log_prob + torch.log(mixprob + 1e-8)).logsumexp(-2)
 
     return -log_probs.mean()
 
@@ -91,12 +112,12 @@ def sample(model, scheduler, num_steps=50, batch_size=128):
 
 
 print('Start training')
-epochs = 100
-pbar = tqdm(total=epochs * len(dataloader))
+pbar = tqdm(total=epochs * len(dataloader), initial=nb_iter)
 # torch.set_float32_matmul_precision("medium")
 # model = torch.compile(model)
 for current_epoch in range(100):
     for i, data in enumerate(dataloader):
+        # with torch.amp.autocast(enabled=True, device_type='cuda', dtype=torch.bfloat16):
         x0 = (data[0].to(device)*2)-1
         noise = torch.randn_like(x0)
         t = torch.randint(0,scheduler.config.num_train_timesteps,(x0.shape[0],),device=device,).long()
@@ -113,9 +134,13 @@ for current_epoch in range(100):
         pbar.update(1)
         pbar.set_description(f'loss: {loss.item()}')
 
-        if nb_iter % 500 == 0:
+        if nb_iter % save_steps == 0:
             with torch.no_grad():
                 print(f'Save export {nb_iter}')
                 outputs = (sample(model, scheduler, num_steps=128, batch_size=128) * 0.5) + 0.5
                 torchvision.utils.save_image(outputs, f'export_{str(nb_iter).zfill(8)}.png')
-                torch.save(model.state_dict(), f'diffusion.ckpt')
+                state_dict = {
+                    "state_dict": model.state_dict(),
+                    "step": nb_iter,
+                }
+                torch.save(state_dict, f'diffusion.ckpt')
